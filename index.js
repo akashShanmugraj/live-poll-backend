a97130652588d00d41ec5015615072a36e451e9d
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2023-08-01 17:07:50 +0530

    first working copy

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const http = require('http').createServer()
+const bodyParser = require("body-parser");
+
 const io = require('socket.io')(http, {
     cors: {origin: "*"}
 });
@@ -7,7 +9,9 @@ console.log("live-poll-backend express")
 console.time()
 const app = express();
 const product = require("./api/product");
+
 app.use(express.json({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/credits", (req, res) => {
     res.send("live-poll-backend express boilerplate from https://github.com/hatuanem199801/express-vercel ")
@@ -17,11 +21,20 @@ app.get("/api", (req, res) => {
     res.send("API for live-poll-backend")
 })
 
+app.post("/socket/:id", (req, res) => {
+    const id = req.params.id
+    var data = JSON.stringify(req.body)
+    console.log(`Sending message ${data} to ${id}`)
+    io.to(id).emit('message', data)
+    res.send(`Sent message ${data} to ${id}`).status(200)
+})
+
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('message', (msg) => {
+        console.log('Added new connection to the room ' + msg)
         console.log(msg);
-        io.emit('message', msg);
+        socket.join(msg)
         console.log(socket.id)
     });
     socket.on('disconnect', () => {
